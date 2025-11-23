ef5c27b2044fd5b994321150b71a63fc4381e49f
murathany7 <49359906+murathany7@users.noreply.github.com>
2025-11-23 22:52:10 +0300

    Update project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -4,8 +4,15 @@ import { useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { StaticImageData } from "next/image";
 
-type ProjectProps = (typeof projectsData)[number];
+type ProjectProps = {
+  title: string;
+  description: string;
+  tags: readonly string[];
+  imageUrl: StaticImageData;
+  link?: string;
+};
 
 export default function Project({
   title,
@@ -24,31 +31,31 @@ export default function Project({
 
   const content = (
     <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:min-h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] flex flex-col sm:group-even:ml-[40%]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-            {description}
-          </p>
-          <ul className="flex flex-wrap mt-4 gap-2">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] flex flex-col sm:group-even:ml-[40%]">
+        <h3 className="text-2xl font-semibold">{title}</h3>
+        <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+          {description}
+        </p>
+        <ul className="flex flex-wrap mt-4 gap-2">
+          {tags.map((tag, index) => (
+            <li
+              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+              key={index}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
 
-        <Image
-          src={imageUrl}
-          alt="Project I worked on"
-          quality={95}
-          style={{ borderRadius: "0.5rem" }}
-          className="absolute hidden sm:block top-8 right-4 w-[16rem] h-[16rem] object-contain transition group-hover:scale-[1.04] group-hover:-rotate-2 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:left-4"
-        />
-      </section>
+      <Image
+        src={imageUrl}
+        alt="Project I worked on"
+        quality={95}
+        style={{ borderRadius: "0.5rem" }}
+        className="absolute hidden sm:block top-8 right-4 w-[16rem] h-[16rem] object-contain transition group-hover:scale-[1.04] group-hover:-rotate-2 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:left-4"
+      />
+    </section>
   );
 
   return (
